5c4e33b4bd5d548e12345563e129cb1107542ec7
Jampan <88382497+Jampan@users.noreply.github.com>
2023-03-03 15:22:11 +0530

    Delete luck.tf

--- luck.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0557a15b87f6559cf"
-  instance_type = "t2.micro"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "HelloWorld-dev"
-    env = "Dev"
-  }
-}
